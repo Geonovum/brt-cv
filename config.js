1ed880dbb11d57dbde9220bb5fc033cee69112d9
Arnoud de Boer <a.deboer@geonovum.nl>
2023-05-26 16:01:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -40,9 +40,9 @@ var respecConfig =
 {
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
   //specStatus: "GN-BASIS",             // Basis Document
-  //specStatus: "GN-WV",              // Werk Versie
+ specStatus: "GN-WV",              // Werk Versie
   //specStatus: "GN-CV",              // Consultatie Versie
-  specStatus: "GN-VV",              // Vaststellings Versie
+  //specStatus: "GN-VV",              // Vaststellings Versie
   //specStatus: "GN-DEF",             // Definitieve Versie
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
   //specType: "NO",                   // Norm
@@ -155,8 +155,8 @@ var respecConfig =
   postProcess:[custGHPG],   //-- Optioneel voor een multi document repository
 
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
-  previousPublishDate: "2023-01-01",  	    // Format is "YYY-MM-DD"
-  previousMaturity: "GN-CV",                   // kies 1 van deze 2 regels  	  
+  previousPublishDate: "2023-05-26",  	    // Format is "YYY-MM-DD"
+  previousMaturity: "GN-VV",                   // kies 1 van deze 2 regels  	  
   //previousMaturity: "VV",  	                // kies 1 van deze 2 regels
 
   //-- Optionele parameters:
